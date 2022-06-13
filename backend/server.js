8641b7c737bd5422b4f238bde425bd09fc309d1a
rajeshg-gintaa <rajesh@asconsoftech.com>
2022-06-13 16:51:20 +0530

    Creating APIs with ExpressJS

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
-const app = express()
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API is Running");
+});
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
 
 app.listen(5000, console.log("Server started on PORT 5000"));
