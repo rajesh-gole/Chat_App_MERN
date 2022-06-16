4f3f60d9c92493b4d44d25a7c6ce75ea517265c4
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-16 18:01:50 +0530

    issue : getting error in login api hit resolved

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,8 @@ const { registerUser, authUser } = require("../controllers/userControllers");
 const router = express.Router()
 
 router.route('/').post(registerUser);
-router.route('/login', authUser)
+router.route('/login').post(authUser);
+// router.route('/login',authUser);
 
 
 module.exports = router;
