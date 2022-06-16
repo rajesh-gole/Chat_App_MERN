1386fdfaac8729951331cf6850f9e86802487afd
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-16 18:17:41 +0530

    API error Handler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 
 dotenv.config();
@@ -18,7 +19,12 @@ app.get('/', (req, res) => {
     res.send("API is Running Successfully");
 });
 
-app.use('/api/user',userRoutes)
+app.use('/api/user', userRoutes)
+
+// API Error Handlers
+app.use(notFound)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000;
 
