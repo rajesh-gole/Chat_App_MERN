388220f4f522998bccd2bc40e461502f56d120c3
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-16 15:36:48 +0530

    user Rourer ( Backend)

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
 
 dotenv.config();
@@ -15,15 +16,7 @@ app.get('/', (req, res) => {
     res.send("API is Running Successfully");
 });
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-});
-
-app.get('/api/chat/:id', (req, res) => {
-    // console.log(req.params.id);
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-});
+app.use('/api/user',userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
