98f149f9fc4ba0594ac9990cc0feac720afad634
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-16 12:04:11 +0530

    Login and SignUp page

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,4 +1,13 @@
-import {Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Show, VStack} from '@chakra-ui/react'
+import {
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Show,
+    VStack
+} from '@chakra-ui/react'
 import React, {useState} from 'react'
 
 const Signup = () => {
@@ -11,6 +20,10 @@ const Signup = () => {
 
     const handleClick = () => setShow(!show);
 
+    const postDetails = (pics) => { };
+
+    const submitHandler = () => { };
+
     return (
         <VStack spacing='5px' color='black'>
             <FormControl id='first-name' isRequired="isRequired">
@@ -20,44 +33,71 @@ const Signup = () => {
 
             <FormControl id='email' isRequired="isRequired">
                 <FormLabel>Email</FormLabel>
-                <Input placeholder='Enter your Email' onChange={(e) => setEmail(e.target.value)}/>
+                <Input
+                    placeholder='Enter your Email'
+                    onChange={(e) => setEmail(e.target.value)}/>
             </FormControl>
 
             <FormControl id='password' isRequired="isRequired">
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
-                <Input
-                    type={show?'text':'password'}
-                    placeholder='Enter your Password'
-                    onChange={(e) => setPassword(e.target.value)} 
-                    
-                    />
+                    <Input
+                        type={show
+                            ? 'text'
+                            : 'password'}
+                        placeholder='Enter your Password'
+                        onChange={(e) => setPassword(e.target.value)}/>
                     <InputRightElement width="4.5rem">
                         <Button h='1.75rem' size='sm' onClick={handleClick}>
-                            {show ? 'Hide' : 'Show'}
+                            {
+                                show
+                                    ? 'Hide'
+                                    : 'Show'
+                            }
                         </Button>
                     </InputRightElement>
 
-                    </InputGroup>
+                </InputGroup>
             </FormControl>
 
             <FormControl id='password' isRequired="isRequired">
                 <FormLabel>Confirm Password</FormLabel>
                 <InputGroup>
-                <Input
-                    type={show?'text':'password'}
-                    placeholder='Confirm Password'
-                    onChange={(e) => setPassword(e.target.value)} 
-                    
-                    />
+                    <Input
+                        type={show
+                            ? 'text'
+                            : 'password'}
+                        placeholder='Confirm Password'
+                        onChange={(e) => setPassword(e.target.value)}/>
                     <InputRightElement width="4.5rem">
                         <Button h='1.75rem' size='sm' onClick={handleClick}>
-                            {show ? 'Hide' : 'Show'}
+                            {
+                                show
+                                    ? 'Hide'
+                                    : 'Show'
+                            }
                         </Button>
                     </InputRightElement>
-
-                    </InputGroup>
+                </InputGroup>
             </FormControl>
+            <FormControl id='pic'>
+                <FormLabel>Upload Profile Picture</FormLabel>
+                <Input
+                    type='file'
+                    p={1.5}
+                    accept='image/*'
+                    onChange={(e) => postDetails(e.target.files[0])}
+                />
+            </FormControl>
+
+            <Button
+                colorScheme='blue'
+                width='100%'
+                style={{ marginTop: 15 }}
+                onClick={submitHandler}
+            >
+                Sign Up
+            </Button>
 
         </VStack>
     )
