a3f88ace0760ab5ebb189ca674285310638bf2ad
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-16 17:42:25 +0530

    Password Encryption with bcryptjs (Backend)

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -40,4 +40,22 @@ const registerUser = asyncHandler(async (req,res) => {
 
 });
 
-module.exports = { registerUser };
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await User.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid Email or Password");
+    }
+});
+
+module.exports = { registerUser, authUser};
